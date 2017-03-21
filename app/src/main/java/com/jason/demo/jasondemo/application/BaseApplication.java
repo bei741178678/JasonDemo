2f69ec649af32da10f297c8ad42d817ce8afc7ef
jason <741178678@qq.com>
2017-03-21 14:13:38 +0800

    demo 创建

---
+++ app/src/main/java/com/jason/demo/jasondemo/application/BaseApplication.java
@@ -0,0 +1,10 @@
+package com.jason.demo.jasondemo.application;
+
+import android.app.Application;
+
+/**
+ * Created by jason on 2017-3-21.
+ */
+
+public class BaseApplication extends Application {
+}
