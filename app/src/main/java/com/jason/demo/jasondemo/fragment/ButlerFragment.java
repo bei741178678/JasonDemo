5be6de0d6c240e427a0c72e8528bfdf9d1686d0b
jason <741178678@qq.com>
2017-03-28 10:38:46 +0800

    删除eventbus moudle
    删除eventbus moudle

--- app/src/main/java/com/jason/demo/jasondemo/fragment/ButlerFragment.java
+++ app/src/main/java/com/jason/demo/jasondemo/fragment/ButlerFragment.java
@@ -16,15 +16,14 @@ public class ButlerFragment extends Fragment {
 
 
     public ButlerFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_butler, container, false);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_butler, container, false);
+        return view;
     }
 
 }
